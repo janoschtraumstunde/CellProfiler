7bfa21a08592836e0b8a0c9d30cfa944388c1f33
Allen Goodman <allen.goodman@icloud.com>
2016-07-28 17:35:51 -0400

    ok

--- cellprofiler/modules/volume.py
+++ cellprofiler/modules/volume.py
@@ -1,4 +1,6 @@
-"""Load3DImage
+"""
+
+Volume
 
 """
 
@@ -6,15 +8,14 @@ import cellprofiler.image
 import cellprofiler.measurement
 import cellprofiler.module
 import cellprofiler.setting
-
+import matplotlib.pyplot
 import os.path
-
 import skimage.io
 
 
-class Load3DImage(cellprofiler.module.Module):
-    module_name = "Load3DImage"
-    category = "Volumetric"
+class Volume(cellprofiler.module.Module):
+    category = "Input/output (I/O)"
+    module_name = "Volume"
     variable_revision_number = 1
 
     def is_3d_load_module(self):
@@ -35,17 +36,15 @@ class Load3DImage(cellprofiler.module.Module):
             0
         )
 
-        self.image_name = cellprofiler.setting.ImageNameProvider(
-            text = "Image name:"
+        self.name = cellprofiler.setting.ImageNameProvider(
+            text = "Name"
         )
-
-        ## TODO: Select channel?
 
     def settings(self):
         return [
             self.directory,
             self.filename,
-            self.image_name,
+            self.name,
             self.channel
         ]
 
@@ -53,18 +52,18 @@ class Load3DImage(cellprofiler.module.Module):
         return [
             self.directory,
             self.filename,
-            self.image_name,
+            self.name,
             self.channel
         ]
 
     def prepare_run(self, workspace):
-        # Pipeline counts image sets from measurements.image_set_count and will raise an error
-        # if there are no image sets (which is apparently the same as no measurements).
+        # Pipeline counts image sets from measurements.image_set_count and will raise an error if there are no image
+        # sets (which is apparently the same as no measurements).
         workspace.measurements.add_measurement(
             cellprofiler.measurement.IMAGE,
             cellprofiler.measurement.C_PATH_NAME,
             os.path.join(self.directory.get_absolute_path(), self.filename.value),
-            image_set_number = 1
+            image_set_number=1
         )
 
         return True
@@ -74,11 +73,13 @@ class Load3DImage(cellprofiler.module.Module):
 
         channel = self.channel.value
 
-        name = self.image_name.value
+        name = self.name.value
 
         data = skimage.io.imread(path)[:, :, :, channel]
 
-        image = cellprofiler.image.Image(dimensions=3)
+        image = cellprofiler.image.Image(
+            dimensions=3
+        )
 
         image.pixel_data = data
 
@@ -88,11 +89,15 @@ class Load3DImage(cellprofiler.module.Module):
             workspace.display_data.image = data
 
     def display(self, workspace, figure):
-        figure.set_subplots((1, 1))
+        image = workspace.display_data.image[15]
 
-        figure.subplot_imshow_grayscale(
+        dimensions = (1, 1)
+
+        figure.set_subplots(dimensions)
+
+        figure.subplot_imshow(
             0,
             0,
-            workspace.display_data.input_pixels[0],
-            ""
+            image,
+            colormap="gray"
         )
